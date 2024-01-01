341f3ae5eb09bef1098c8b46480d54244e6074ba
Rascal_Two <therealrascaltwo@gmail.com>
2024-01-01 09:19:49 -0600

    Add pre-commit and remove poethepoet

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,9 @@
 from questdrive_syncer.example import example
 
 
-def test_normal():
+def test_normal() -> None:
     assert example(1, 2) == 3
+
+
+def test_extra() -> None:
+    assert example(2, 2) == 6
