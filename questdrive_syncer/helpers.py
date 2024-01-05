7e3a8cc5d104481f52e138a94f68d060109b3b9d
Rascal_Two <therealrascaltwo@gmail.com>
2024-01-05 07:16:46 -0600

    Fix FileNotFound error when output path is missing

--- questdrive_syncer/helpers.py
+++ questdrive_syncer/helpers.py
@@ -1,11 +1,13 @@
 """Collection of helper functions."""
 import os
+from pathlib import Path
 
 from .constants import MINIMUM_FREE_SPACE_BYTES, OUTPUT_PATH
 
 
 def has_enough_free_space(mb_size: float) -> bool:
     """Return if there is enough free space to download the video."""
+    Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
     statvfs = os.statvfs(OUTPUT_PATH)
     free_space = statvfs.f_frsize * statvfs.f_bavail
     free_space_after_download = free_space - mb_size * 1024**2
--- questdrive_syncer/test_helpers.py
+++ questdrive_syncer/test_helpers.py
@@ -6,6 +6,15 @@ from pytest_mock import MockerFixture
 from .helpers import has_enough_free_space
 
 
+def test_has_enough_free_space_calls_mkdir(mocker: MockerFixture) -> None:
+    """has_enough_free_space() returns True when there is enough free space."""
+    mock_mkdir = mocker.patch("pathlib.Path.mkdir")
+
+    has_enough_free_space(0)
+
+    mock_mkdir.assert_called_once_with(parents=True, exist_ok=True)
+
+
 def test_has_enough_free_space_enough(mocker: MockerFixture) -> None:
     """has_enough_free_space() returns True when there is enough free space."""
     mocker.patch(
